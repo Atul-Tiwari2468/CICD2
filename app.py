e16d1840795367bef1c2068ac2b2e06ec8156c10
Atul-Tiwari2468 <atultiwari40@yahoo.com>
2026-07-29 16:56:13 +0530

    Add GitHub Actions CI workflow

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ def about():
     return "👋 My name is Atul. I am learning Flask, Docker, GitHub Actions, and Render."
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
