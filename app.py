d963d7e27014b8a3ae145c2cf766143bfbb7cf74
Atul-Tiwari2468 <atultiwari40@yahoo.com>
2026-07-31 01:41:32 +0530

    Add health check endpoint

--- app.py
+++ app.py
@@ -10,5 +10,10 @@ def home():
 def about():
     return "👋 My name is Atul. I am learning Flask, Docker, GitHub Actions, and Render."
 
+@app.route("/healthz")
+def health():
+    return "OK", 200
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
+
